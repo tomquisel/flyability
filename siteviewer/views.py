95d428ec8cc62dbc774f4ade23f91f7c0933d336
Tom Quisel <tom.quisel@gmail.com>
2012-09-03 22:43:11 -0400

    Small improvements.
    Remove unneeded bootstrap file
    start adding help popover
    remove dependency of matplotlib on x server

--- siteviewer/views.py
+++ siteviewer/views.py
@@ -2,6 +2,8 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext, loader
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 from siteviewer.models import Site
