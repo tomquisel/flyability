57af1e87419911efb4b6b11e7a60b5ecd1cae32b
Tom Quisel <tom.quisel@gmail.com>
2012-11-13 01:15:33 -0500

    Filter out stub sites, better distance sort

--- siteviewer/views.py
+++ siteviewer/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.cache import cache_control
 from siteviewer.models import Site
 import siteviewer.main as main
 import grapher, multigrapher
+from math import sin, cos, atan2, pi
 
 def index(request):
     env = {}
@@ -51,17 +52,41 @@ def allSiteNames(request):
 def distCmp(s1, s2, lat, lon):
     return cmp(dist(s1, lat, lon), dist(s2, lat, lon))
 
+def d2r(a):
+    return a / 180 * pi
+
+def mi2km(x):
+    return 1.60934 * x
+
 def dist(s, lat, lon):
-    return ((s.lat-lat)**2 + (s.lon-lon)**2)**0.5
+    lats = d2r(s.lat)
+    lons = d2r(s.lon)
+    latf = d2r(lat)
+    lonf = d2r(lon)
+    # From http://en.wikipedia.org/wiki/Great-circle_distance
+    dlat = abs(lats - latf)
+    dlon = abs(lons - lonf)
+    n1 = (cos(latf)*sin(dlon))**2
+    n2 = (cos(lats)*sin(latf) - (sin(lats)*cos(latf)*cos(dlon)))**2
+    d = sin(lats)*sin(latf) + cos(lats)*cos(latf)*cos(dlon)
+    return atan2((n1 + n2)** 0.5, d) * 3963.1676
 
 
 def search(request):
     lat = float(request.GET['lat'])
     lon = float(request.GET['lon'])
-    sites = list(Site.objects.all())
+    query = Site.objects.all().exclude(
+                takeoffObj='[[0, 360, "no"]]'
+            )
+    sites = list(query)
     sites.sort(lambda s1, s2: distCmp(s1, s2, lat, lon))
     for s in sites:
-        setattr(s, 'dist', dist(s, lat, lon))
+        dmi = dist(s, lat, lon)
+        dkm = mi2km(dmi)
+        dmi = int(round(dmi))
+        dkm = int(round(dkm))
+        setattr(s, 'dist_mi', dmi)
+        setattr(s, 'dist_km', dkm)
     env = { 'sites' : sites[:20] }
     return render_to_response('siteviewer/search.html', env,
                               context_instance=RequestContext(request))
